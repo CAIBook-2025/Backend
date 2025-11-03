0bd08cf63d96b4d7cb915e09db4773dcb6e6e94c
TRINIDAD MUNOZ <t.munozp@uc.cl>
2025-11-02 22:14:06 -0300

    updated scheduling endpoints

--- src/users/usersService.js
+++ src/users/usersService.js
@@ -2,11 +2,10 @@ const { prisma } = require('../lib/prisma');
 
 class UsersService {
   async createUser(userData) {
-
-    console.log('ENTRA ACÁ');
     const user = await prisma.user.create({
       data: {...userData}
     });
+
     return user; 
   }
 }
