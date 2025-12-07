dd745921a376ce9c4c4db076c3bf67565e7fe017
Cristobal Donoso Maass <cristobaldonosomaass@uc.cl>
2025-12-07 19:08:02 -0300

    feat: update schedules retrieval to include user instead of student

--- src/routes/schedule.routes.js
+++ src/routes/schedule.routes.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
         orderBy: [{ day: 'asc' }, { module: 'asc' }],
         include: {
           studyRoom: true,
-          student: true
+          user: true
         }
       }),
       prisma.sRScheduling.count({ where })
